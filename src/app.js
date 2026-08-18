e04a2f697fff6b87b2f920773644c2d4ffa28e27
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-08-18 15:38:51 +0300

    refactor: if else x3

--- src/app.js
+++ src/app.js
@@ -26,27 +26,25 @@ if (!fs.statSync(sourceFile).isFile()) {
 const path = require('path');
 const fileName = path.basename(sourceFile);
 const fileRoad = path.join(destinationDir, fileName);
+const isDirectory =
+  fs.existsSync(destinationDir) && fs.statSync(destinationDir).isDirectory();
 
-if (destinationDir.endsWith('/')) {
-  if (fs.existsSync(destinationDir)) {
-    fs.renameSync(sourceFile, fileRoad);
-  } else {
-    console.error('Destination directory does not exist.');
-    process.exit(0);
-  }
-} else if (
-  fs.existsSync(destinationDir) &&
-  fs.statSync(destinationDir).isDirectory()
-) {
+if (destinationDir.endsWith('/') && !isDirectory) {
+  console.error('Destination directory does not exist.');
+  process.exit(1);
+}
+
+if (isDirectory) {
   fs.renameSync(sourceFile, fileRoad);
   process.exit(0);
-} else {
-  const pathParent = path.dirname(destinationDir);
-
-  if (!fs.existsSync(pathParent)) {
-    console.error('Parent directory does not exist.');
-    process.exit(0);
-  }
-  fs.renameSync(sourceFile, destinationDir);
-  process.exit(0);
 }
+
+const pathParent = path.dirname(destinationDir);
+
+if (!fs.existsSync(pathParent)) {
+  console.error('Parent directory does not exist.');
+  process.exit(1);
+}
+
+fs.renameSync(sourceFile, destinationDir);
+process.exit(0);
